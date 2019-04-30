1aa349f1b9e22a2c8e5879b9f859b24dd7e8b9e2
Zach Butler <zach.standley93@gmail.com>
2019-04-30 09:04:32 -0700

    travis changes

--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
 
-/* uncomment for DOM api support
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM(``);
 global.window = window;
 global.document = window.document;
-*/
+global.FormData = window.FormData;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
